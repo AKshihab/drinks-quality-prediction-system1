9db6b8547ff1d76f02fcc73008a872873a980b00
AKshihab <shihabkaiyumhossen@gmail.com>
2026-07-28 05:20:32 +0600

    s

--- week4_php_auth/logout.php
+++ week4_php_auth/logout.php
@@ -1,1 +1,23 @@
+<?php
+session_start();
 
+$_SESSION = [];
+
+if (ini_get('session.use_cookies')) {
+    $cookie_parameters = session_get_cookie_params();
+
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $cookie_parameters['path'],
+        $cookie_parameters['domain'],
+        $cookie_parameters['secure'],
+        $cookie_parameters['httponly']
+    );
+}
+
+session_destroy();
+
+header('Location: login.php?msg=' . urlencode('You have logged out successfully.'));
+exit();
